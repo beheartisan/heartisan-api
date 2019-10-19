9027e4f00e2cbf17051cb8fdfabbc8c3b1ad054a
Venkatesh Panyam <venkatesh@habba.org>
2019-10-19 13:30:22 +0530

    initial commit

---
+++ src/main/java/com/heartisan/HeartisanApplication.java
@@ -0,0 +1,13 @@
+package com.heartisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartisanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartisanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heartisan/HeartisanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartisan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartisanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
