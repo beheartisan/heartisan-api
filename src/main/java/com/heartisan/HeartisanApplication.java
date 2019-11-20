5f3c8b307da187de16f79bedf4a36244b321bc22
Venkatesh Panyam <venkatesh@rangdelabs.in>
2019-11-20 23:28:56 +0530

    Add SpringApplicationBuilder for war building

--- src/main/java/com/heartisan/HeartisanApplication.java
+++ src/main/java/com/heartisan/HeartisanApplication.java
@@ -2,11 +2,17 @@ package com.heartisan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 public class HeartisanApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application;
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HeartisanApplication.class, args);
